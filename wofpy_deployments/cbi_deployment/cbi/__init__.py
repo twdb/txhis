66f5dc63a1b87f72d7a8633573793c5f060f2ea3
wilsaj <wilson.andrew.j+github@gmail.com>
2011-10-03 12:17:03 -0500

    fix paths; add a DEPLOYED hook but it's not too robust - should integrate this into the config file at some point

--- wofpy_deployments/cbi_deployment/cbi/__init__.py
+++ wofpy_deployments/cbi_deployment/cbi/__init__.py
@@ -2,22 +2,22 @@ import logging
 import os
 import tempfile
 
-import soaplib
-from soaplib.core.server import wsgi
-from werkzeug.wsgi import DispatcherMiddleware
 import wof
 
 from cbi_dao import CbiDao
 
-# CBI_DEPLOYMENT_DIR = '/space/www/wofpy_deployments/cbi_deployment'
-# CBI_CACHE_DIR = os.path.join(CBI_DEPLOYMENT_DIR,
-#                              'cache/')
-CBI_DEPLOYMENT_DIR = './'
-CBI_CACHE_DIR = tempfile.gettempdir()
+DEPLOYED = True
 
-CBI_CONFIG_FILE = os.path.join(CBI_DEPLOYMENT_DIR,
-                               'cbi_config.cfg')
-CBI_CONFIG_FILE = 'cbi_config.cfg'
+if DEPLOYED:
+    CBI_DEPLOYMENT_DIR = '/space/www/wofpy_deployments/cbi_deployment'
+    CBI_CACHE_DIR = os.path.join(CBI_DEPLOYMENT_DIR,
+                                 'cache/')
+    CBI_CONFIG_FILE = os.path.join(CBI_DEPLOYMENT_DIR,
+                                   'cbi_config.cfg')
+else:
+    CBI_DEPLOYMENT_DIR = './'
+    CBI_CACHE_DIR = tempfile.gettempdir()
+    CBI_CONFIG_FILE = 'cbi_config.cfg'
 
 CBI_CACHE_DATABASE_URI = 'sqlite:////' + os.path.join(
     CBI_CACHE_DIR, 'cbi_dao_cache.db')
@@ -27,4 +27,5 @@ logging.basicConfig(level=logging.DEBUG)
 cbi_dao = CbiDao(CBI_CONFIG_FILE, database_uri=CBI_CACHE_DATABASE_URI)
 app = wof.create_wof_app(cbi_dao, CBI_CONFIG_FILE)
 
-app.run()
+if not DEPLOYED:
+    app.run()
