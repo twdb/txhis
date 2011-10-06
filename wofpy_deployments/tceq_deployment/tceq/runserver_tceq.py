adc2d225a71f30aff08011ca855b98aa47103919
wilsaj <wilson.andrew.j+github@gmail.com>
2011-10-06 11:59:20 -0500

    use simpler wofpy create_wof_app() function to initialize the tceq  service WSGI app

--- wofpy_deployments/tceq_deployment/tceq/runserver_tceq.py
+++ wofpy_deployments/tceq_deployment/tceq/runserver_tceq.py
@@ -1,40 +1,17 @@
 import logging
 
-import soaplib
-from soaplib.core.server import wsgi
-from werkzeug.wsgi import DispatcherMiddleware
-
-from wof import WOF
-from wof.soap import create_wof_service_class
-from wof.flask import create_app, config
+import wof
 
 from pyhis_dao import PyhisDao
 
 logging.basicConfig(level=logging.DEBUG)
 
-
 TCEQ_DATABASE_URI = 'sqlite:////home/wilsaj/txhis/import_data/cache_files/tceq_pyhis_cache.db'
+TCEQ_CONFIG_FILE ='tceq_config.cfg'
 
-dao = PyhisDao(TCEQ_DATABASE_URI,
+tceq_dao = PyhisDao(TCEQ_DATABASE_URI,
                'tceq_config.cfg')
-
-tceq_wof = WOF(dao)
-tceq_wof.config_from_file('tceq_config.cfg')
-
-app = create_app(tceq_wof)
-app.config.from_object(config.DevConfig)
-
-TCEQWOFService = create_wof_service_class(tceq_wof)
-
-soap_app = soaplib.core.Application(services=[TCEQWOFService],
-                                    tns='http://www.cuahsi.org/his/1.0/ws/',
-                                    name='WaterOneFlow')
-
-soap_wsgi_app = soaplib.core.server.wsgi.Application(soap_app)
-
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/soap/tceq': soap_wsgi_app
-    })
+app = wof.create_wof_app(tceq_dao, TCEQ_CONFIG_FILE)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True)
